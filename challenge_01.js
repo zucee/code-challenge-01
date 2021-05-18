52a7390b48383ff52f510ab6cfbd14539ecc1d01
zucee <kujon@gmx.net>
2021-05-18 21:42:55 +0200

    https://github.com/zucee/code-challenge-01.git

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,10 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
+
+var firstNumber = prompt("Bitte geben Sie eine Zahl ein:");
+var secondNumber = prompt ("Bitte geben Sie eine zweite Zahl ein:");
+
+var sum = (firstNumber*1 + secondNumber*1);
+
+console.log("Die Summe der Zahlen ist: " + sum);
